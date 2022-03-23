533b931f477f0a4540e2b43e768f88edf7b53a3d
Andrix <andrixtan_96@hotmail.com>
2022-03-23 17:58:59 +0800

    Finish project

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 const mainContainer = document.querySelector(".main-container");
 
-let NumberOfGrids = 50;
+let randomColor = () => {
+	let randomNumber = Math.floor(Math.random() * 16777215).toString(16);
+	return `#${randomNumber}`;
+};
 
-const createDivs = () => {
+const createDivs = (NumberOfGrids) => {
 	for (let j = 0; j < NumberOfGrids; j++) {
-		let createColDiv = document.createElement('div');
+		let createColDiv = document.createElement("div");
 		createColDiv.className = "column-div";
 		for (let i = 0; i < NumberOfGrids; i++) {
-			// console.log(i);
 			let createSquareDiv = document.createElement("div");
+			createSquareDiv.style.backgroundColor = 'white';
 			createSquareDiv.className = "square-div";
 			createSquareDiv.onmouseover = function () {
-				createSquareDiv.style.backgroundColor = 'red';
-			}
+				if (createSquareDiv.style.backgroundColor === 'white') {
+					createSquareDiv.style.backgroundColor = randomColor();
+				} else if (createSquareDiv.style.filter === 'brightness(0%)') {
+					return
+				} else if (createSquareDiv.style.filter) {
+					let currentBrightness = parseInt(createSquareDiv.style.filter.slice(11,13));
+					let newBrightness = `brightness(${currentBrightness - 10}%)`;
+					createSquareDiv.style.filter = newBrightness;
+				} else {
+					createSquareDiv.style.filter = 'brightness(90%)'
+				}
+			};
 			createColDiv.appendChild(createSquareDiv);
 		}
+		resetButton.textContent = 'Reset'
 		mainContainer.appendChild(createColDiv);
-	};
+	}
 };
-createDivs();
+
+const resetDrawing = () => {
+	let NumberOfGrids = prompt("Please key in grid size");
+	while (isNaN(NumberOfGrids) || NumberOfGrids === '' || NumberOfGrids > 100) {
+		if (NumberOfGrids === null) {
+			return
+		}
+		NumberOfGrids = prompt("Invalid. Please key in valid number lesser than 100.");
+	}
+	if (isNaN(parseInt(NumberOfGrids))) {
+		return
+	} else {
+		while (mainContainer.firstChild) {
+			mainContainer.removeChild(mainContainer.firstChild);
+		}
+		createDivs(NumberOfGrids);
+	}
+};
+
+const resetButton = document.querySelector(".reset-button");
+resetButton.addEventListener("click", (e) => {
+	resetDrawing();
+});
+
+// resetDrawing();
